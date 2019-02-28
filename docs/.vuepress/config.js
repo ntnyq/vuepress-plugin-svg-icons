dd964f7ce57f3a73255d520653c7161692485d16
ntnyq <ntnyq13@gmail.com>
2019-02-28 18:08:25 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,10 +4,6 @@ module.exports = {
   title: 'Vuepress-plugin-svg-icons',
   description: '在 Vuepress 中使用 SVG sprite icon',
   dest: 'site',
-<<<<<<< HEAD
-=======
-  base: '/vuepress-plugin-svg-icons/',
->>>>>>> cdb6a0a... Update config.js
   locals: {
     '/': {
       lang: 'zh-CN',
