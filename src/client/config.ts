67482d199e215b98c761c520120822c345190cb0
ntnyq <ntnyq13@gmail.com>
2026-07-01 11:19:10 +0800

    chore: update

--- src/client/config.ts
+++ src/client/config.ts
@@ -1,9 +1,9 @@
 import './style.css'
 import { h } from 'vue'
 import { defineClientConfig } from 'vuepress/client'
-import { Icon, Sprites } from './components/index.js'
-import type { SvgIconPropsOptions } from '../shared/index.js'
-import type { IconProps } from './components/index.js'
+import { Icon, Sprites } from './components'
+import type { SvgIconPropsOptions } from '../shared'
+import type { IconProps } from './components'
 
 declare const __SVG_ICON_ID_PREFIX__: string
 declare const __SVG_ICON_COMPONENT_NAME__: string
